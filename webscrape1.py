b2ef71e403ca7d44c440023860a462376114d66d
djs21905 <djs21905@gmail.com>
2017-04-24 20:22:31 -0400

    Cleaned up some of the redundant code

--- webscrape1.py
+++ webscrape1.py
@@ -24,13 +24,7 @@ for data in table_data:
                 if len(name.text) > 5 and 'Rutgers' not in name.text:
                         raw_names.append(name.text)
 
-
 seperated_names = [item.split(' ') for item in raw_names] 
-print(seperated_names)
-
-
-
-
 
 # Write data to Excel file
 wb = openpyxl.load_workbook('test.xlsx')
@@ -38,21 +32,14 @@ sheet = wb.get_sheet_by_name('Sheet1')
 
 rowNum = sheet.max_row + 1 
 for seperated_name in seperated_names:
-        print(seperated_name)
+        sheet.cell(row = rowNum, column = 1).value = rowNum - 1
+        sheet.cell(row = rowNum, column = 2).value = seperated_name[0]
         if len(seperated_name) == 2:
-                sheet.cell(row = rowNum, column = 1).value = rowNum - 1
-                sheet.cell(row = rowNum, column = 2).value = seperated_name[0]
                 sheet.cell(row = rowNum, column = 3).value = seperated_name[1]
-        elif len(seperated_name) == 3:
-                sheet.cell(row = rowNum, column = 1).value = rowNum - 1
-                sheet.cell(row = rowNum, column = 2).value = seperated_name[0]
+        if len(seperated_name) >= 3 and len(seperated_name)<4:
                 sheet.cell(row = rowNum, column = 3).value = seperated_name[2]
                 sheet.cell(row = rowNum, column = 4).value = seperated_name[1]
         elif len(seperated_name) >= 4:
-                sheet.cell(row = rowNum, column = 1).value = rowNum - 1
-                sheet.cell(row = rowNum, column = 2).value = seperated_name[0]
-                sheet.cell(row = rowNum, column = 3).value = seperated_name[2]
-                sheet.cell(row = rowNum, column = 4).value = seperated_name[1]
                 sheet.cell(row = rowNum, column = 5).value = seperated_name[3]
         rowNum += 1
 
