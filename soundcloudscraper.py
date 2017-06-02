7f77aeeb2556f619fb74caf0c6f696790848d166
djs21905 <djs21905@gmail.com>
2017-06-02 17:09:14 -0400

    Test

--- soundcloudscraper.py
+++ soundcloudscraper.py
@@ -1,6 +1,7 @@
 # Sound Cloud Scraper
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 # User input for artist name/ song name
@@ -12,7 +13,6 @@ r = requests.get("https://soundcloud.com/search?q=" + search_query)
 # Converts the html request object to a soup object
 soup = BeautifulSoup(r.text)
 
-
 links = soup.find_all('h2')
 
 final_list = []
@@ -20,12 +20,19 @@ for item in links:
     link = item.find('a')
     final_list.append(link.get('href'))
 
-
-
 minutes = input("Enter minute: ")
 seconds = input("Enter seconds: ")
 
 print("https://soundcloud.com" + final_list[0] + '#t=' + minutes + 'm' + seconds+ 's')
+
+r2 = requests.get("https://soundcloud.com" + final_list[0] + '#t=' + minutes + 'm' + seconds+ 's')
+test = re.findall(r'soundcloud.com/tracks/\d{3,9}', r2.text)
+
+for item in test:
+    print(item)
+
+
+
 
 
 
