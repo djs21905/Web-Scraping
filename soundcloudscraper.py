badf5ec2da32d1b8c625723625d2af495b2ba3b8
djs21905 <djs21905@gmail.com>
2017-06-02 12:17:54 -0400

    SC scraper

---
+++ soundcloudscraper.py
@@ -0,0 +1,34 @@
+# Sound Cloud Scraper
+import requests
+from bs4 import BeautifulSoup
+
+
+# User input for artist name/ song name
+search_query = input("Enter search query: ")
+
+# Creates an http req and retrieves the url based on the artist/ song name
+r = requests.get("https://soundcloud.com/search?q=" + search_query)
+
+# Converts the html request object to a soup object
+soup = BeautifulSoup(r.text)
+
+
+links = soup.find_all('h2')
+
+final_list = []
+for item in links:
+    link = item.find('a')
+    final_list.append(link.get('href'))
+
+
+
+minutes = input("Enter minute: ")
+seconds = input("Enter seconds: ")
+
+print("https://soundcloud.com" + final_list[0] + '#t=' + minutes + 'm' + seconds+ 's')
+
+
+
+
+
+
